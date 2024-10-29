7cbdc7d66adea36c4c93818e1dc0c00b9320482e
DeedleFake <deedlefake@users.noreply.github.com>
2024-10-28 22:48:58 -0400

    add `:thumbnex`

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule Pleb.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:thumbnex, "~> 0.5"}
     ]
   end
 
