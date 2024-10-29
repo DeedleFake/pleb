230a95f073db601a3f6fdea4b562d58e0ae52a72
DeedleFake <deedlefake@users.noreply.github.com>
2024-10-28 23:50:24 -0400

    add `:dialyxir`

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule Pleb.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:ffmpex, "~> 0.11"}
+      {:ffmpex, "~> 0.11"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
